844eb232df842a038dd8cf4fe44a04625438f9b5
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-09-02 18:44:13 +0300

    Completed one app for simple math operations.

---
+++ Essentials/MathOperations/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+solve(5, 6, '/');
+
+
+function solve(num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '/': result = num1 / num2; break;
+        case '*': result = num1 * num2; break;
+        case '%': result = num1 % num2; break;
+        case '**': result = num1 ** num2; break;
+    }
+    console.log(result);
+}
+console.exception("stop")
+
